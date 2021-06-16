901c8080e3484d5430fdbfc2cb0fbf8cec645023
code860 <benjaminc.barber@gmail.com>
2021-06-16 12:02:21 -0400

    Switched .secrets to .credentials when looking up the redis sidekiq url in encrypted secrets

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-REDIS_SIDEKIQ_URL= ENV.fetch('CURATOR_APP_SIDEKIQ_REDIS_URL') { Rails.application.secrets.dig(:redis, :sidekiq_url) }
+REDIS_SIDEKIQ_URL= ENV.fetch('CURATOR_APP_SIDEKIQ_REDIS_URL') { Rails.application.credentials.dig(:redis, :sidekiq_url) }
 
 Sidekiq.configure_client do |config|
   config.redis = { url: REDIS_SIDEKIQ_URL }
