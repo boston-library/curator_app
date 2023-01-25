11e78d417a8dc57ae474008d595ab4831b635015
bbarberBPL <bbarber@bpl.org>
2023-01-25 09:33:52 -0500

    Housekeeping(added frozen string literals)
    updated solr configs submodule

--- config/initializers/azure.rb
+++ config/initializers/azure.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'azure_storage_client_overrides'
-
 Rails.application.reloader.to_prepare do
   Azure::Storage::Common::Client.include(AzureStorageClientOverrides::CoreClientOverride)
   Azure::Storage::Blob::BlobService.include(AzureStorageClientOverrides::BlobStorageClientOverride)
