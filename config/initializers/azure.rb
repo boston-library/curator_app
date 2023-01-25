6a7f96dd7f20d74e4708fa8915ca59b734879978
bbarberBPL <bbarber@bpl.org>
2023-01-25 16:48:26 -0500

    A little more housekeeping(frozen string literals)
    Use threads when doing multiple put block blob uploads

--- config/initializers/azure.rb
+++ config/initializers/azure.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.reloader.to_prepare do
+  require 'azure_storage_client_overrides'
+
   Azure::Storage::Common::Client.include(AzureStorageClientOverrides::CoreClientOverride)
   Azure::Storage::Blob::BlobService.include(AzureStorageClientOverrides::BlobStorageClientOverride)
 end
