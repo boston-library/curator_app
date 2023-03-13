4cd85730e704bafe3a2b5c75d799661b6661785d
miao-bpl <rmiao@bpl.org>
2023-03-13 11:10:19 -0400

    Test capistrano

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-	APP_INFO = {
-    app_name: 'bpldc_authority_api',
-    organization: 'Boston Public Library',
-    version: '1.0',
-  }.freeze
 end
