e53846bd999fcb7d6860b011d1d7f47ccb5417ca
bbarberBPL <bbarber@bpl.org>
2023-04-26 16:41:28 -0400

    Updated sidekiq and puma

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,5 +8,6 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = { url: REDIS_SIDEKIQ_URL }
-  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
+  config.logger = Sidekiq::Logger.new($stdout)
+  config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
 end
