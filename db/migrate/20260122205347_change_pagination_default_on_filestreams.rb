7704777067e9ff71db9d6f43a8880d21d83bc0b6
bbarberBPL <bbarber@bpl.org>
2026-01-30 09:50:36 -0500

    Changed migration to update all file sets pagination to new default

--- db/migrate/20260122205347_change_pagination_default_on_filestreams.rb
+++ db/migrate/20260122205347_change_pagination_default_on_filestreams.rb
@@ -2,7 +2,7 @@
 
 class ChangePaginationDefaultOnFilestreams < ActiveRecord::Migration[7.2]
   def change
-
     change_column_default :filestreams_file_sets, :pagination, from: '{}', to: {}
+    Curator::Filestreams::FileSet.where("pagination = ?", '{}').update_all(pagination: {})
   end
 end
